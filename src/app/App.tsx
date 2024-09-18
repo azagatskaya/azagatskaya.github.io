6a13a5810735214d6bc02db37cf5184290d29b9c
Alya Zagatskaya <alya.zagacki@gmail.com>
2024-09-18 10:56:57 +0300

    add: info

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,9 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
-        </p>
+        <div style={{ width: 800 }}>
+          <p>Меня зовут Аля Загацкая, я джуниор фронтенд разработчик, работаю с NextJs, React Native.</p>
+          <p>
+            С помощью этого курса хочу повысить свой уровень, больше узнать про сборку, архитектуру проекта, best
+            practicies в React.
+          </p>
+        </div>
       </header>
     </div>
   );
